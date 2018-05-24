085810461de1cbb60b5c66a1893bf22ce90577dc
James Shields <james@lostcarpark.com>
2018-05-24 11:05:54 +0100

    Changed to used Ajax calls to speed loading.

---
+++ js/brick_filter.js
@@ -0,0 +1,22 @@
+Drupal.behaviors.brickFilterBehavior = function (context)  {
+  $('.brick-filter-part').each(function () {
+    $(this).removeClass('brick-filter-part').addClass('brick-filter-part-processed');
+    var part = $(this).attr("part");
+    var color = $(this).attr("color");
+    var element = this;
+    $.get('/brick_filter/part/'+part+'/color/'+color, function(data) {
+      element.innerHTML = data;
+      Drupal.attachBehaviors($('.brick-filter-part-processed'));
+    });
+  });
+  $('.brick-filter-set').each(function () {
+    $(this).removeClass('brick-filter-set').addClass('brick-filter-set-processed');
+    var set = $(this).attr("set");
+    var element = this;
+    $.get('/brick_filter/set/'+set, function(data) {
+      element.innerHTML = data;
+      Drupal.attachBehaviors($('.brick-filter-set-processed'));
+    });
+  });
+};
+
